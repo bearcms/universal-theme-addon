069b8c31e8c58a8fd62c78388d21704192ca61ee
Ivo Petkov <ivo@ivopetkov.com>
2019-03-05 13:44:28 +0200

    Version update (because of bug in bearcms/bearframework-addon).

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $app->bearCMS->themes
             $context->assets
             ->addDir('assets');
 
-            $theme->version = '1.4';
+            $theme->version = '1.5';
 
             $theme->get = function(\BearCMS\Themes\Theme\Customizations $customizations) use ($app, $context) {
                 $templateFilename = $context->dir . '/components/defaultTemplate.php';
