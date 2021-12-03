9e0b9845580e9bda2c386c6afea2eb3056e3b6e3
Ivo Petkov <ivo@ivopetkov.com>
2021-12-03 14:02:12 +0200

    Add unit tests.

---
+++ tests/UniversalThemeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Universal Theme addon for Bear CMS
+ * https://github.com/bearcms/universal-theme-addon
+ * Copyright (c) Amplilabs Ltd.
+ * Free to use under the MIT license.
+ */
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class UniversalThemeTest extends BearCMS\AddonTests\PHPUnitTestCase
+{
+    /**
+     * 
+     */
+    public function testTheme()
+    {
+        $app = $this->getApp();
+
+        $this->assertTrue(true);
+    }
+}
